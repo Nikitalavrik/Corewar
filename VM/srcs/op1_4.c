f5292ffc514f01929449d167b929f39f1c946bb7
Nikita <worldmixgames@gmail.com>
2019-09-09 18:30:01 +0300

    write "add", but not tested

--- VM/srcs/op1_4.c
+++ VM/srcs/op1_4.c
@@ -6,7 +6,7 @@
 /*   By: nlavrine <nlavrine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/09 12:28:01 by nlavrine          #+#    #+#             */
-/*   Updated: 2019/09/09 18:17:37 by nlavrine         ###   ########.fr       */
+/*   Updated: 2019/09/09 18:29:35 by nlavrine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,7 +81,32 @@ void	ft_st(t_cw *corewar, t_cursor *cursor, t_op op)
 
 void	ft_add(t_cw *corewar, t_cursor *cursor, t_op op)
 {
+	unsigned char	arg1;
+	unsigned char	arg2;
+	unsigned char	arg3;
+	unsigned char	type;
+
 	ft_printf("process %s nbr_players %i cursor id %i\n",\
 	op.func_name, corewar->player_nbr, cursor->id);
+	type = corewar->map[cursor->position + 1];
+	arg1 = type >> 6;
+	arg2 = (char)(type << 2) >> 6;
+	arg3 = (char)(type << 4) >> 6;
+	if (arg1 != REG_CODE || arg2 != REG_CODE || arg2 != REG_CODE)
+	{
+		cursor->position++;
+		return ;
+	}
+	arg1 = grep_args(corewar->map, cursor->position + 2, T_REG);
+	arg2 = grep_args(corewar->map, cursor->position + 2, T_REG);
+	arg3 = grep_args(corewar->map, cursor->position + 2, T_REG);
+	if (arg1 > 16 || arg2 > 16 || arg3 > 16)
+	{
+		cursor->position++;
+		return ;
+	}
+	cursor->reg[arg3] = cursor->reg[arg1] + cursor->reg[arg2];
+	cursor->carry = !cursor->reg[arg3] ? 1 : cursor->carry;
+	cursor->position += (2 + 3 * T_REG);
 	return ;
 }
