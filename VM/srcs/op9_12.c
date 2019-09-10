1bd39c037ed27c50819578ec0bea7b57d6823db9
Nikita <worldmixgames@gmail.com>
2019-09-10 17:53:32 +0300

    write ldi and sti

--- VM/srcs/op9_12.c
+++ VM/srcs/op9_12.c
@@ -6,7 +6,7 @@
 /*   By: nlavrine <nlavrine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/09 12:32:55 by nlavrine          #+#    #+#             */
-/*   Updated: 2019/09/10 15:41:30 by nlavrine         ###   ########.fr       */
+/*   Updated: 2019/09/10 17:53:01 by nlavrine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,15 +37,15 @@ void	ft_ldi(t_cw *corewar, t_cursor *cursor, t_op op)
 	out_func_info(corewar, cursor, op);
 	type = corewar->map[cursor->position + 1];
 	type_arg1 = type >> 6;
-	type_arg2 = (char)(type << 2) >> 6;
-	type = (char)(type << 4) >> 6;
+	type_arg2 = (unsigned char)(type << 2) >> 6;
+	type = (unsigned char)(type << 4) >> 6;
 	arg1 = check_grep_args(corewar->map, cursor->position + 2, type_arg1, op.t_dirsize);
-	arg2 = check_grep_args(corewar->map, cursor->position +\
+	arg2 = check_grep_args(corewar->map, cursor->position + 2 +\
 					get_val_size(type_arg1, op.t_dirsize), type_arg2, op.t_dirsize);
 	if (type_arg1 == REG_CODE && arg1 && arg1 <= 16)
 		arg1 = cursor->reg[arg1 - 1];
 	else if (type_arg1 == IND_CODE)
-		arg1 = check_grep_args(corewar->map, cursor->position + 2\
+		arg1 = check_grep_args(corewar->map, cursor->position\
 											+ arg1 % IDX_MOD, IND_CODE, op.t_dirsize);
 	if (type_arg2 == REG_CODE && arg2 && arg2 <= 16)
 		arg2 = cursor->reg[arg2 - 1];
@@ -55,16 +55,50 @@ void	ft_ldi(t_cw *corewar, t_cursor *cursor, t_op op)
 		arg3 = grep_args(corewar->map, cursor->position + 2 +\
 		get_val_size(type_arg1, op.t_dirsize) +\
 		get_val_size(type_arg2, op.t_dirsize), T_REG);
-		cursor->reg[arg3 - 1] = (arg1 + arg2) % IDX_MOD;
+		if (arg3 && arg3 <= 16)
+			cursor->reg[arg3 - 1] = cursor->position + (arg1 + arg2) % IDX_MOD;
 	}
 	cursor->position += (2 + get_val_size(type_arg1, op.t_dirsize) +\
 	get_val_size(type_arg2, op.t_dirsize) + get_val_size(type, op.t_dirsize));
+	dump(cursor);
 }
 
 void	ft_sti(t_cw *corewar, t_cursor *cursor, t_op op)
 {
+	int	arg1;
+	int	arg2;
+	unsigned char	arg3;
+	unsigned char	type_arg1;
+	unsigned char	type_arg2;
+	unsigned char	type;
+
 	out_func_info(corewar, cursor, op);
-	return ;
+	type = corewar->map[cursor->position + 1];
+	type_arg1 = type >> 6;
+	type_arg2 = (unsigned char)(type << 2) >> 6;
+	type = (unsigned char)(type << 4) >> 6;
+	arg1 = check_grep_args(corewar->map, cursor->position + 2, type_arg1, op.t_dirsize);
+	arg2 = check_grep_args(corewar->map, cursor->position + 2 + \
+					get_val_size(type_arg1, op.t_dirsize), type_arg2, op.t_dirsize);
+	arg3 = check_grep_args(corewar->map, cursor->position + 2 +\
+						get_val_size(type_arg1, op.t_dirsize) +\
+						get_val_size(type_arg1, op.t_dirsize), type_arg2, op.t_dirsize);
+	if (type_arg2 == REG_CODE && arg2 && arg2 <= 16)
+		arg2 = cursor->reg[arg2 - 1];
+	else if (type_arg2 == IND_CODE)
+		arg2 = check_grep_args(corewar->map, cursor->position\
+											+ arg2 % IDX_MOD, IND_CODE, op.t_dirsize);
+	if (type == REG_CODE && arg3 && arg3 <= 16)
+		arg3 = cursor->reg[arg3 - 1];
+
+	if (type_arg1 == REG_CODE && type_arg1 && type_arg2 && type != IND_CODE)
+	{
+		if (arg1 && arg1 <= 16)
+			cursor->reg[arg1 - 1] = cursor->position + (arg2 + arg3) % IDX_MOD;
+	}
+	cursor->position += (2 + get_val_size(type_arg1, op.t_dirsize) +\
+	get_val_size(type_arg2, op.t_dirsize) + get_val_size(type, op.t_dirsize));
+	dump(cursor);
 }
 
 void	ft_fork(t_cw *corewar, t_cursor *cursor, t_op op)
