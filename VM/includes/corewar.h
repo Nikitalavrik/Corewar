482b8b50d2f3a313b8ce5b007b7d10493f59b59f
Nikita <worldmixgames@gmail.com>
2019-10-14 13:38:09 +0300

    norme .h file

--- VM/includes/corewar.h
+++ VM/includes/corewar.h
@@ -6,7 +6,7 @@
 /*   By: nlavrine <nlavrine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/05 15:47:47 by tbratsla          #+#    #+#             */
-/*   Updated: 2019/10/14 13:13:26 by nlavrine         ###   ########.fr       */
+/*   Updated: 2019/10/14 13:37:44 by nlavrine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,6 @@ typedef struct		s_cursor
 	int				id;
 	int				carry;
 	int				op;
-	int				is_wait;
 	int				cycles_num;
 	int				remaining_cycles;
 	int				position;
@@ -83,7 +82,7 @@ typedef	struct		s_op
 
 typedef union		u_type
 {
-	unsigned char 	types;
+	unsigned char	types;
 	struct			s_tp
 	{
 		unsigned char	t4 : 2;
@@ -93,84 +92,90 @@ typedef union		u_type
 	}				t_tp;
 }					t_type;
 
-/**
- ** print function
- */
-void			print_error(char *error);
-void			out_players(t_player *players);
-void			out_cursor(t_cursor *cursor);
-void			out_print_bytes(unsigned char *line, int count);
-void			out_func_info(t_cw *corewar, t_cursor *cursor, t_op op);
-void			dump(unsigned char *map);
+/*
+** print function
+*/
 
-/**
- ** parse function
- */
-t_player		*parse_argv(t_cw *corewar, int argc, char ** argv);
-t_cw			*parse_file(t_cw	*corewar, t_player *players);
-int				check_uniq_id(t_player *players, int id);
-void			check_uniq_name(t_player *players);
-int				choose_flag(t_cw *corewar, char **argv, int *pos, int argc);
+void				print_error(char *error);
+void				out_players(t_player *players);
+void				out_cursor(t_cursor *cursor);
+void				out_print_bytes(unsigned char *line, int count);
+void				out_func_info(t_cw *corewar, t_cursor *cursor, t_op op);
+void				dump(unsigned char *map);
 
-/**
- ** main cycle function
- */
-void			engine(t_cw *corewar);
-int				check_operation(t_cw *corewar, t_cursor *cursor, int op);
-unsigned int 	place_cur(int cur);
-t_player 		check_winner(t_player *players, int n);
-int				do_op(t_cw *corewar, t_cursor *cursor);
+/*
+** parse function
+*/
 
-/**
- ** grep value from map
- */
-unsigned int	get_val_size(int type, char t_dirsize);
-int				grep_args(unsigned char *map, int position, int size);
-int				check_grep_args(unsigned char *map, int position,\
+t_player			*parse_argv(t_cw *corewar, int argc, char **argv);
+t_cw				*parse_file(t_cw *corewar, t_player *players);
+int					check_uniq_id(t_player *players, int id);
+void				check_uniq_name(t_player *players);
+int					choose_flag(t_cw *corewar, char **argv, int *pos, int argc);
+
+/*
+** main cycle function
+*/
+
+void				engine(t_cw *corewar);
+int					check_operation(t_cw *corewar, t_cursor *cursor, int op);
+unsigned int		place_cur(int cur);
+t_player			check_winner(t_player *players, int n);
+int					do_op(t_cw *corewar, t_cursor *cursor);
+
+/*
+** grep value from map
+*/
+
+unsigned int		get_val_size(int type, char t_dirsize);
+int					grep_args(unsigned char *map, int position, int size);
+int					check_grep_args(unsigned char *map, int position,\
 													int type, char t_dirsize);
-int				*init_args(t_cw *corewar, t_cursor *cursor,\
+int					*init_args(t_cw *corewar, t_cursor *cursor,\
 														t_op op, t_type type);
 
-/**
- ** cursor function
- */
-void			copy_cursor(t_cursor *src, t_cursor *dist);
-void			add_cursor(t_cw *cw, int i);
-void			cursor(t_cw *cw);
-int				del_cursor(t_cursor **cursor, t_cursor **prev,\
-											t_cursor **main_cursor, t_cw *corewar);
-/**
- ** visual function
- */
-void			vis_init(t_cw *corewar);
-void			draw_map(t_cw *corewar);
-void  			set_player_collor(int i, t_cw *corewar, WINDOW *win);
-void  			reset_player_collor(int i, t_cw *corewar);
-void 			draw_player(unsigned char *area, int i,\
-				unsigned int prog_size,	t_cw *corewar);
-void  			draw_cursor(int pos, t_cursor *start, t_cw *corewar,\
-													t_cursor *cursor);
-void    		set_color_by_cursor(t_cw *corewar, t_cursor *cursor);
-void    		cursor_color_to_player(int pos, t_cw *corewar);
-void   			set_cursor_color_by_pos(t_cw *corewar, t_cursor *start);
-void    		ft_init_colors(void);
-void    		draw_box_and_words(t_cw *corewar);
-void			draw_box_and_words2(t_cw *corewar);
-void			draw_box(t_cw *corewar);
-void    		draw_player_name(char *prog_name, t_cw *corewar);
-void			after_game(t_cw *corewar, t_player player, char c);
+/*
+** cursor function
+*/
 
-/**
- ** implement func
- */
+void				copy_cursor(t_cursor *src, t_cursor *dist);
+void				add_cursor(t_cw *cw, int i);
+void				cursor(t_cw *cw);
+int					del_cursor(t_cursor **cursor, t_cursor **prev,\
+								t_cursor **main_cursor, t_cw *corewar);
+/*
+** visual function
+*/
 
-void			ft_and_xor_or(t_cw *corewar, t_cursor *cursor,\
+void				vis_init(t_cw *corewar);
+void				draw_map(t_cw *corewar);
+void				set_player_collor(int i, t_cw *corewar, WINDOW *win);
+void				reset_player_collor(int i, t_cw *corewar);
+void				draw_player(unsigned char *area, int i,\
+							unsigned int prog_size, t_cw *corewar);
+void				draw_cursor(int pos, t_cursor *start, t_cw *corewar,\
+														t_cursor *cursor);
+void				set_color_by_cursor(t_cw *corewar, t_cursor *cursor);
+void				cursor_color_to_player(int pos, t_cw *corewar);
+void				set_cursor_color_by_pos(t_cw *corewar, t_cursor *start);
+void				ft_init_colors(void);
+void				draw_box_and_words(t_cw *corewar);
+void				draw_box_and_words2(t_cw *corewar);
+void				draw_box(t_cw *corewar);
+void				draw_player_name(char *prog_name, t_cw *corewar);
+void				after_game(t_cw *corewar, t_player player, char c);
+
+/*
+** implement func
+*/
+
+void				ft_and_xor_or(t_cw *corewar, t_cursor *cursor,\
 										t_op op, int f(int, int));
-void			ft_add_sub(t_cw *corewar, t_cursor *cursor,\
+void				ft_add_sub(t_cw *corewar, t_cursor *cursor,\
 										t_op op, int f(int, int));
-int				calc_pos(t_type	type, int n, t_op op);
+int					calc_pos(t_type	type, int n, t_op op);
 
 extern			t_op	g_op_tab[17];
-int		g_id;
-int		g_i;
+int				g_id;
+int				g_i;
 #endif
