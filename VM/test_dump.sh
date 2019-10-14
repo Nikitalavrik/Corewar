72cc4477dd7d677836b43efda58dd3df7969176f
Tima <timamaestro@gmail.com>
2019-10-14 14:30:10 +0300

    Norm

--- VM/test_dump.sh
+++ VM/test_dump.sh
@@ -6,14 +6,21 @@ read to
 
 for  i in $(seq $from $to)
 do
-	./corewar examples/asm/test.cor -d $i > test_toto
-	../../resources/corewar examples/asm/test.cor -d $i > test_original
-	dif=$(diff test_toto test_original)
-	echo $i >> test_diff
-	diff test_toto test_original >> test_diff
-	# if [ $dif ]
-	# then
-	# 	echo $i >> differ
-	# 	echo $dif >> differ
-	# fi
+	# ./corewar examples/toto.cor -d $i > test_toto
+	# ../../resources/corewar examples/toto.cor -d $i > test_original
+	# dif=$(diff test_toto test_original)
+	# echo $i >> test_diff
+	# diff test_toto test_original >> test_diff
+	# # if [ $dif ]
+	# # then
+	# # 	echo $i >> differ
+	# # 	echo $dif >> differ
+	# # fi
+	# ./corewar examples/.cor -d $i > test_toto
+	# ../../resources/corewar examples/toto.cor -d $i > test_original
+	./corewar ../../examples/toto.cor -d $i > test_ex
+	../../../vm_champs/corewar ../../../vm_champs/champs/toto.cor -d $i > test_original
+	echo $i >> differ
+	diff test_ex test_original >> differ
 done
+afplay music/winner.mp3&
