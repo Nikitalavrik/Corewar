ec90cfcd74dc48445fb86f142760a7fcc78abdc4
Nikita <worldmixgames@gmail.com>
2019-10-17 15:17:07 +0300

    fix carry flag

--- VM/test_dump.sh
+++ VM/test_dump.sh
@@ -6,21 +6,15 @@ read to
 
 for  i in $(seq $from $to)
 do
-	# ./corewar examples/toto.cor -d $i > test_toto
-	# ../../resources/corewar examples/toto.cor -d $i > test_original
-	# dif=$(diff test_toto test_original)
-	# echo $i >> test_diff
-	# diff test_toto test_original >> test_diff
-	# # if [ $dif ]
-	# # then
-	# # 	echo $i >> differ
-	# # 	echo $dif >> differ
-	# # fi
-	# ./corewar examples/.cor -d $i > test_toto
-	# ../../resources/corewar examples/toto.cor -d $i > test_original
-	./corewar ../../examples/toto.cor -d $i > test_ex
-	../../../vm_champs/corewar ../../../vm_champs/champs/toto.cor -d $i > test_original
-	echo $i >> differ
-	diff test_ex test_original >> differ
+	./corewar examples/toto.cor examples/maxidef.cor examples/jumper.cor -d $i > test_toto
+	../../resources/corewar examples/toto.cor examples/maxidef.cor examples/jumper.cor -d $i > test_original
+	dif=$(diff test_toto test_original)
+	echo $i >> test_diff
+	diff test_toto test_original >> test_diff
+	# if [ $dif ]
+	# then
+	# 	echo $i >> differ
+	# 	echo $dif >> differ
+	# fi
 done
-afplay music/winner.mp3&
+
