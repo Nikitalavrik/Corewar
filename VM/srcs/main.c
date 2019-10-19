21387bff4e57b463ed7a696607e7f5c8e494f864
Nikita <worldmixgames@gmail.com>
2019-10-19 12:28:43 +0300

    add beauty output

--- VM/srcs/main.c
+++ VM/srcs/main.c
@@ -6,15 +6,21 @@
 /*   By: nlavrine <nlavrine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/02 16:17:00 by nlavrine          #+#    #+#             */
-/*   Updated: 2019/10/18 16:37:03 by nlavrine         ###   ########.fr       */
+/*   Updated: 2019/10/19 12:26:48 by nlavrine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
 
-void	print_usage(void)
+void	print_usage(char *name)
 {
-	ft_printf("./corewar [-dump nbr_cycles] [[-n number] champion1.cor] ...\n");
+	ft_printf("%s [[-n number] champion.cor -dump N | -v | -o | -l | -a] ...\n\
+	\t-a\t: show aff output\n\
+	\t-dump N\t: dump memory after N cycles\n\
+	\t-l\t: show lives\n\
+	\t-o\t: show operation and number of cycle\n\
+	\t-v\t: vizualization\n\
+	\t-n N\t: num of player\n", name);
 	exit(0);
 }
 
@@ -25,7 +31,7 @@ int		main(int argc, char **argv)
 
 	g_id = 1;
 	if (argc < 2)
-		print_usage();
+		print_usage(argv[0]);
 	corewar = ft_memalloc(sizeof(t_cw));
 	corewar->cycle_to_die = CYCLE_TO_DIE;
 	players = parse_argv(corewar, argc, argv);
