0d37f466637ca47af64edebb3da2d3df9101421d
Nikita <worldmixgames@gmail.com>
2019-10-19 17:33:34 +0300

    Auto stash before merge of "master" and "origin/master"

--- VM/testing/test_dump.sh
+++ VM/testing/test_dump.sh
@@ -7,15 +7,12 @@ read to
 
 for  i in $(seq $from $to)
 do
-	../corewar ../examples/sam_2.0.cor ../examples/loose.cor -dump $i > test_toto
-	/Users/nlavrine/Documents/resources/corewar ../examples/sam_2.0.cor ../examples/loose.cor -d $i > test_original
+	../corewar ../champs/championships/2014/gudepard/Kappa.cor ../champs/championships/2014/srabah-m/littlepuppy.cor ../champs/championships/2014/fcorre/ultimate-surrender.cor ../champs/examples/zork.cor -dump $i > test_toto
+	/Users/nlavrine/Documents/resources/corewar  ../champs/championships/2014/gudepard/Kappa.cor ../champs/championships/2014/srabah-m/littlepuppy.cor ../champs/championships/2014/fcorre/ultimate-surrender.cor ../champs/examples/zork.cor -d $i > test_original
+	# ../corewar ../champs/championships/2014/clafleur/big_feet.cor ../champs/bot_kompot.cor ../champs/examples/bigzork.cor ../champs/championships/2014/clafleur/big_feet.cor -dump $i > test_toto
+	# /Users/nlavrine/Documents/resources/corewar ../champs/championships/2014/clafleur/big_feet.cor ../champs/bot_kompot.cor ../champs/examples/bigzork.cor ../champs/championships/2014/clafleur/big_feet.cor -d $i > test_original
 	dif=$(diff test_toto test_original)
 	echo $i >> test_diff
 	diff test_toto test_original >> test_diff
-	# if [ $dif ]
-	# then
-	# 	echo $i >> differ
-	# 	echo $dif >> differ
-	# fi
 done
 
